25ba41a02b34cc6c993b84dd9d8ca181ef298b65
ninah <malalaninah6@gmail.com>
2025-07-19 19:59:40 +0300

    natsoina use effect le service

--- react-geo-app/react-geo-app/src/App.js
+++ react-geo-app/react-geo-app/src/App.js
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react";
+import { getAllCountries } from "./services/countryService";
+import MapView from "./components/MapView";
+
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getAllCountries()
+      .then((data) => {
+        console.log("Pays azo=> ", data);  
+        setCountries(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <div className="App">
       <h1>React Geo App By ninah</h1>
+      <MapView countries={countries}/>
     </div>
   );
 }
